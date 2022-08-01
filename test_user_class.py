6bca2297a9e6ee0219edf99951232ca82916dd53
Cameron Avellani <59744801+Camavellani@users.noreply.github.com>
2022-08-01 17:46:39 -0400

    Update test_user_class.py

--- test_user_class.py
+++ test_user_class.py
@@ -22,23 +22,23 @@ class test_user_class(user):
            test_value = 10
        cursor.execute("""DELETE FROM COURSE WHERE CRN = ?;""", (crn,))
        return(test_value)      # Returns 10 if a course is successfully added, returns 0 if not
-    def test_login(self, user_choice, first, last, id):
+    def test_login(self, user_choice, first, last, password):
         # first = input("Enter your first name: \n")
         # last = input("Enter your last name: \n")
         # id = input("Enter your ID number without the W: \n")
         test_value = 0
         if(user_choice == 1):
             # Verify student credentials
-            cursor.execute("""SELECT ID FROM STUDENT WHERE ID = ?""", (id,))
+            cursor.execute("""SELECT PASSWORD FROM STUDENT WHERE PASSWORD = ?""", (password,))
             query_result1 = cursor.fetchall()
             cursor.execute("""SELECT FIRST FROM STUDENT WHERE FIRST = ?""", (first,))
             query_result2 = cursor.fetchall()
             cursor.execute("""SELECT LAST FROM STUDENT WHERE LAST = ?""", (last,))
             query_result3 = cursor.fetchall()
             if(len(query_result1) == 0):
-                result_id = "er404"
+                result_password = "er404"
             else:
-                result_id = id
+                result_password = password
             if(len(query_result2) == 0):
                 result_first = "er404"
             else:
@@ -50,16 +50,16 @@ class test_user_class(user):
             
         elif(user_choice == 2):
             # Verify instructor credentials
-            cursor.execute("""SELECT ID FROM INSTRUCTOR WHERE ID = ?""", (id,))
+            cursor.execute("""SELECT PASSWORD FROM INSTRUCTOR WHERE PASSWORD = ?""", (password,))
             query_result1 = cursor.fetchall()
             cursor.execute("""SELECT FIRST FROM INSTRUCTOR WHERE FIRST = ?""", (first,))
             query_result2 = cursor.fetchall()
             cursor.execute("""SELECT LAST FROM INSTRUCTOR WHERE LAST = ?""", (last,))
             query_result3 = cursor.fetchall()
             if(len(query_result1) == 0):
-                result_id = "er404"
+                result_password = "er404"
             else:
-                result_id = id
+                result_password = password
             if(len(query_result2) == 0):
                 result_first = "er404"
             else:
@@ -71,16 +71,16 @@ class test_user_class(user):
             
         elif(user_choice == 3):
             # Verify admin credentials
-            cursor.execute("""SELECT ID FROM ADMIN WHERE ID = ?""", (id,))
+            cursor.execute("""SELECT PASSWORD FROM ADMIN WHERE PASSWORD = ?""", (password,))
             query_result1 = cursor.fetchall()
             cursor.execute("""SELECT FIRST FROM ADMIN WHERE FIRST = ?""", (first,))
             query_result2 = cursor.fetchall()
             cursor.execute("""SELECT LAST FROM ADMIN WHERE LAST = ?""", (last,))
             query_result3 = cursor.fetchall()
             if(len(query_result1) == 0):
-                result_id = "er404"
+                result_password = "er404"
             else:
-                result_id = id
+                result_password = password
             if(len(query_result2) == 0):
                 result_first = "er404"
             else:
@@ -94,9 +94,9 @@ class test_user_class(user):
             print("That was not a valid input. Please try again.\n")
             result_first = "er404"
             result_last = "er404"
-            result_id = "er404"
+            result_password = "er404"
 
         # Successful Login
-        if(id == result_id) and (first == result_first) and (last == result_last):
+        if(password == result_password) and (first == result_first) and (last == result_last):
             test_value = 10
         return(test_value)      # Returns 10 if log in successful, returns 0 if not
